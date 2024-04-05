463955a1afc8e8b97fe158c420194bd5a6d78d14
Niamreg76 <84070314+Niamreg76@users.noreply.github.com>
2024-04-05 16:03:26 +0200

    test arbre

--- parser/RecipeTree.cs
+++ parser/RecipeTree.cs
@@ -3,5 +3,12 @@
     public class RecipeTree(String Token)
     {
         public String Token { get; set; } = Token;
+
+        public List<RecipeTree> SubRecipeTrees { get; set; } = [];
+
+        public void AddSubRecipe(RecipeTree SubRecipeTree)
+        {
+            SubRecipeTrees.Add(SubRecipeTree);
+        }
     }
 }
